a033b962d9ed95107dd6b8eade37cc8a15992394
luizvelasco <luizvelasco@gmail.com>
2023-08-22 09:08:33 -0300

    aula 67

--- core/ConfigController.php
+++ core/ConfigController.php
@@ -4,17 +4,63 @@ namespace Core;
 
 class ConfigController
 {
-    public string $url;
+    private string $url;
+    private array $urlArray;
+    private string $urlController;
+    private string $urlParameter;
+    private string $urlSlugController;
+    private array $format;
 
     public function __construct()
     {
         echo "Carregar a página<hr>";
         if (!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
             $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
-            var_dump($this->url);
+
+            $this->clearUrl();
+
+            $this->urlArray = explode("/", $this->url);
+
+            if (isset($this->urlArray[0])){
+                $this->urlController = $this->slugController($this->urlArray[0]);
+            } else {
+                $this->urlController = "Home";
+            }
         } else {
             echo "Acessa a página inicial"; 
+            $this->urlController = "Home";
         }
-       
+
+        echo "Controller: {$this->urlController}<br>";
     }
+
+    private function clearUrl()
+    {
+        // Eliminar as tags
+        $this->url = strip_tags($this->url);
+
+        // Eliminar espaços em branco
+        $this->url = trim($this->url);
+
+        // Eliminar a barra no final da URL
+        $this->url = rtrim($this->url, "/");
+    }
+
+    private function slugController($slugController)
+    {
+        // Converter para minusculo
+        $this->urlSlugController = strtolower($slugController);
+
+        // Converter espaço em branco
+        $this->urlSlugController = str_replace("-", " ", $this->urlSlugController);
+
+        // Converter a primeira letra de cada palavra para maiusuculo
+        $this->urlSlugController = ucwords($this->urlSlugController);
+
+        // REtirar espaco em branco
+        $this->urlSlugController = str_replace(" ", "", $this->urlSlugController);
+
+        return $this->urlSlugController;
+    }
+    
 }
